4cfeb78b87527bcfab4fd3f6696dbc88e8e4631d
mastertech <sundayodum@gmail.com>
2021-07-11 09:26:54 +0100

    added index.css

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import React from 'react'
+import ReactDom from 'react-dom'
 
+function BookList(){
+return <section><Book /></section>;
+}
+
+const Book = ()=> {
+  return ( 
+    <article>
+      <Image></Image>
+      <Title />
+      <Author />
+      </article> 
+    );
+
+};
+const Image= ()=><img src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg" alt="" />
+
+const Title = () => <h1>i love you to the moon back</h1>
+const Author = () =><h4>Amelia Hepworth</h4>
+
+ReactDom.render(<BookList />, document.getElementById('root')); 
